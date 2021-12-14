3ea264ff1096cba3080d2fb7ddeb229549aeb64f
mwmlln <mwmlln@gmail.com>
2021-12-14 21:55:15 +0000

    ADD: Product admin setup and models created and fixture files replaced with utf filetype

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+    Tag, Category, Product, ProductImages
+)
 
-# Register your models here.
+
+class TagInline(admin.TabularInline):
+    model = Product.tags.through
+ 
+
+class ImageInLine(admin.TabularInline):
+    model = ProductImages
+
+
+class ImageAdmin(admin.ModelAdmin):
+    raw_id_field = ['product']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [TagInline, ImageInLine]
+    exclude = ['tags']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(
+    [Tag, Category]
+)
+admin.site.register(ProductImages, ImageAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=32)
+ 
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImages(models.Model):
+    image = models.ImageField(upload_to='')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.IntegerField()
+
+    class Mata:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.product.name + ': ' + str(self.order)
