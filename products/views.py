6f6d4aa4d6e0c286b23b5ffddb2798b48357bb63
mwmlln <mwmlln@gmail.com>
2021-12-15 13:44:34 +0000

    ADD:keyword search function in navbar completed

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
+from django.http import request
 from django.shortcuts import render
+from django.contrib import messages
 from django.views.generic import ListView
+from django.db.models import Q
 from django.views.generic.detail import DetailView
 from .models import Product
 
@@ -7,6 +10,17 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     template_name = 'products/products.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+ 
+        if query:
+            object_list = Product.objects.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+                )
+        else:
+            object_list = Product.objects.all()
+        return object_list
 
 
 class ProductDetaiView(DetailView):
