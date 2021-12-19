dbdf91b6c60be86e910d4acb98b7406b47e6966c
mwmlln <mwmlln@gmail.com>
2021-12-19 19:53:09 +0000

    ADD: category links with background images in index page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Category
 
-def index(request):
-    """ A view to return the index page """
+# def index(request):
+#     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+#     return render(request, 'home/index.html')
+
+class HomeView(ListView):
+    model = Category
+    template_name = 'home/index.html'
