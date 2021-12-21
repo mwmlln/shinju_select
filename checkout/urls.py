417c85fe8622a81c932c1c363063506cf1468332
mwmlln <mwmlln@gmail.com>
2021-12-21 19:45:05 +0000

    ADD: checkout success logic

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,8 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path(
+        'checkout_success/<order_number>',
+        views.checkout_success,
+        name='checkout_success'),
 ]
