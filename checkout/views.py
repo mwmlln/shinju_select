aeb314433b605b16339bb7711d2db57a1679d54b
mwmlln <mwmlln@gmail.com>
2021-12-21 14:23:06 +0000

    ADD: stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
+import stripe
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
 
     bag = request.session.get('bag', {})
     if not bag:
@@ -15,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
--- shinju_select/settings.py
+++ shinju_select/settings.py
@@ -183,3 +183,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
