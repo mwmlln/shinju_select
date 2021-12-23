f2c6151630096879279b2a4504d3ac9d69fe6f1f
mwmlln <mwmlln@gmail.com>
2021-12-23 19:09:48 +0000

    ADD: about and delivery info pages with minimum contents to start with

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('delivery/', views.DeliveryView.as_view(), name='delivery')
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 from products.models import Category
 
 
 class HomeView(ListView):
     model = Category
     template_name = 'home/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+
+class DeliveryView(TemplateView):
+    template_name = 'home/delivery.html'
