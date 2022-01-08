9a7684f5e8a574308e66dbf21de5127c0315ebf8
mwmlln <mwmlln@gmail.com>
2022-01-08 12:51:40 +0000

    REMOVE: image upload in the product management page as it causes errors

--- products/views.py
+++ products/views.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView
 from django.db.models import Q
 from django.views.generic.detail import DetailView
 from .models import Product, Category, Tag
-from review.models import Review
 from .forms import ProductForm, ImageForm, DeleteProductForm
 
 
@@ -57,18 +56,6 @@ class ProductDetaiView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
 
-    # def get(self, request, pk, *args, **kwargs):
-    #     product = get_object_or_404(Product, pk=pk)
-    #     print(f'product:{product}')
-    #     reviews = Review.objects.filter(product=product)
-    #     return render(
-    #                 request,
-    #                 "products/product_detail.html",
-    #                 {
-    #                 "product": product,
-    #                 "reviews": reviews
-    #                 },
-    #             )
 
 @login_required
 def add_product(request):
@@ -80,12 +67,8 @@ def add_product(request):
      
     if request.method == 'POST':
         product_form = ProductForm(request.POST or None)
-        # image_form = ImageForm(request.POST, request.FILES)
         if product_form.is_valid() and image_form.is_valid():
             product = product_form.save()
-            # images = image_form.save(commit=False)
-            # images.product = product
-            # images.save()
             messages.success(request, 'Successfully added product!')
             return redirect(
                         reverse('products:product_detail', 
@@ -100,12 +83,10 @@ def add_product(request):
 
     else:
         product_form = ProductForm()
-        # image_form = ImageForm()
 
     template = 'products/add_product.html'
     context = {
         'product_form': product_form,
-        # 'image_form': image_form
     }
 
     return render(request, template, context)
@@ -121,10 +102,8 @@ def edit_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         product_form = ProductForm(request.POST, instance=product)
-        # image_form = ImageForm(request.POST, request.FILES, instance=product)
         if product_form.is_valid():
             product_form.save()
-            # image_form.save()
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse(
                                 'products:product_detail',
@@ -137,13 +116,11 @@ def edit_product(request, product_id):
                         )
     else:
         product_form = ProductForm(instance=product)
-        # image_form = ImageForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
 
     template = 'products/edit_product.html'
     context = {
         'product_form': product_form,
-        # 'image_form': image_form,
         'product': product,
     }
 
